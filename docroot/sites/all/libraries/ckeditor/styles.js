b5b10d072dcd15df55224b8f5d25e2b60ed7a4fd
Albert Volkman <albert@27squared.com>
2013-09-03 12:55:21 -0400

    Downgrade library: ckeditor 4.03
    https://drupal.org/node/1950636
    
    node/19962

--- docroot/sites/all/libraries/ckeditor/styles.js
+++ docroot/sites/all/libraries/ckeditor/styles.js
@@ -1,6 +1,6 @@
 ﻿/**
  * Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
- * For licensing, see LICENSE.md or http://ckeditor.com/license
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
  */
 
 // This file contains style definitions that can be used by CKEditor plugins.
@@ -57,7 +57,8 @@ CKEDITOR.stylesSet.add( 'default', [
 	{ name: 'Superscript',		element: 'sup' },
 	*/
 
-	{ name: 'Marker',			element: 'span', attributes: { 'class': 'marker' } },
+	{ name: 'Marker: Yellow',	element: 'span', styles: { 'background-color': 'Yellow' } },
+	{ name: 'Marker: Green',	element: 'span', styles: { 'background-color': 'Lime' } },
 
 	{ name: 'Big',				element: 'big' },
 	{ name: 'Small',			element: 'small' },
