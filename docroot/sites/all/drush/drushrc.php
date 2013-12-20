80f72525573f8db9ae3d74e073281b2d9d51a324
Albert Volkman <albert@27squared.com>
2013-12-20 10:29:45 -0500

    Update drush alias files

---
+++ docroot/sites/all/drush/drushrc.php
@@ -0,0 +1,33 @@
+<?php
+
+$options['structure-tables']['common'] = array(
+  'cache',
+  'cache_block',
+  'cache_bootstrap',
+  'cache_field',
+  'cache_filter',
+  'cache_form',
+  'cache_image',
+  'cache_libraries',
+  'cache_location',
+  'cache_media_xml',
+  'cache_menu',
+  'cache_page',
+  'cache_path',
+  'cache_token',
+  'cache_update',
+  'cache_views',
+  'cache_views_data',
+  'ctools_css_cache',
+  'ctools_object_cache',
+  'history',
+  'search_dataset',
+  'search_index',
+  'search_node_links',
+  'search_total',
+  'sessions',
+  'watchdog',
+);
+// Use the list of cache tables above.
+$options['structure-tables-key'] = 'common';
+
